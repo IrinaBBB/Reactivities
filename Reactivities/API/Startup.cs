03b73980b79ddf12288834fdfdde0552e12b8662
Irina <balkarovairina@gmail.com>
2021-10-01 23:40:44 +0200

    End of Section 3

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -32,6 +32,13 @@ namespace API
             {
                 opts.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opts =>
+            {
+                opts.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +54,7 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
