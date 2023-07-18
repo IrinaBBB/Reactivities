3c884851d9aad76cef4175886b90b7a12d4bd20a
IrinaBBB <balkarovairina@gmail.com>
2023-07-18 20:21:28 +0200

    End of section 3 (v.2)

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import {Header, List, ListItem} from 'semantic-ui-react'
 
 function App() {
+    const [activities, setActivities] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities').then((response) => {
+            setActivities(response.data)
+        })
+    }, [])
+
     return (
-        <p>Hello</p>
+        <>
+            <Header as="h2" icon="users" content="Reactivities" />
+            <List>
+                {activities.map((activity) => (
+                    // @ts-ignore
+                    <ListItem key={activity.id}>{activity.title}</ListItem>
+                ))}
+            </List>
+            <button className="ui button">
+                Cancel
+            </button>
+        </>
     )
 }
 
-export default App;
+export default App
